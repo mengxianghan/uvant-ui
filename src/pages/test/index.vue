260afe2f62f86b93531b8700088b64063c3a6c27
MengXianghan <1056811341@qq.com>
2024-10-10 10:38:14 +0800

    feat: 调整全局配置,增加 test 页面

---
+++ src/pages/test/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <view class="p-3">
+        <input
+            v-model="content"
+            type="text"
+            class="border border-solid border-neutral-200 px-2 py-2 rounded-md bg-white"
+            @input="debounceInput" />
+        <view class="mt-3">content:{{ content }}</view>
+    </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { debounce } from 'lodash-es'
+
+const content = ref('')
+
+const debounceInput = debounce(onInput, 100)
+
+function formatter(value) {
+    return value.replace(/\d/g, '')
+}
+
+function onInput(e) {
+    content.value = formatter(e.detail.value)
+}
+</script>
+
+<style lang="scss" scoped></style>
